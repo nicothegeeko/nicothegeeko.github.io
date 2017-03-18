d95477ccccfd9db60e24ac874c8eba1450af2782
Nico Flicker <Nico@Nicos-MacBook-Air.local>
2017-03-18 13:09:03 -0700

    adding html design elements

---
+++ GifTastic/README.md
@@ -0,0 +1,25 @@
+#GifTastic Homework
+
+#Live Link 
+* https://nicothegeeko.github.io/GifTastic
+
+#Requirements
+Sizes needed to demonstrate responsiveness:
+
+ Use the GIPHY API to make a dynamic web page that populates with gifs of your choice.
+
+* When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+* When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+* Under every gif, display its rating (PG, G, so on).
+*Add a form to your page takes the value from a user input box and adds it into your topics array. Then make a function call that takes each topic in the array remakes the buttons on the page.
+
+Technologies Used
+
+* HTML
+* CSS
+*JavaScript
+*JSON
+*GIPHY API
+
+Code Explaination
+* Utilized the giphyAPI to "get" gifs and added the pause/play function by clicking each gif. 
--- GifTastic/assets/gif.js
+++ GifTastic/assets/gif.js
@@ -1,94 +1,122 @@
 $(document).ready(function() {
 
 
-// Array of Topics
+    // Array of Topics
 
-      var topics = ["Afghan Hound", "Akita", "American Eskimo Dog", "American Foxhound", "American Pitbull Terrior",
-"Australian Cattle Dog", "Australian Shephard", "Barbet", "Basenji", "Basset Hound","Beagle",
-"Bearded Collie", "Bichon Frise", "Bolognese", "Border Collie", "Border Terrier", "Boxer", "Bulldog", 
-"Corgi","Chinook", "Chow Chow", "Dachshund", "Dalmatian", "French Bulldog", "White German Shepherd", "Havanese",
-"Jack Russel Terrier", "Jindo Dog", "Labradoodle", "Maltese", "Papillon","Pekingese","Pomeranian", "Pug",
-"Samoyed","Siberian Husky", "Swedish Vallhund", "Yorkipoo"];
+    var topics = ["Afghan Hound", "Akita", "American Eskimo Dog", "American Foxhound", "American Pitbull Terrior",
+        "Australian Cattle Dog", "Australian Shephard", "Barbet", "Basenji", "Basset Hound", "Beagle",
+        "Bearded Collie", "Bichon Frise", "Bolognese", "Border Collie", "Border Terrier", "Boxer", "Bulldog",
+        "Corgi", "Chinook", "Chow Chow", "Dachshund", "Dalmatian", "French Bulldog", "White German Shepherd", "Havanese",
+        "Jack Russel Terrier", "Jindo Dog", "Labradoodle", "Maltese", "Papillon", "Pekingese", "Pomeranian", "Pug",
+        "Samoyed", "Siberian Husky", "Swedish Vallhund", "Yorkipoo"
+    ];
 
-      function renderButtons() {
+    function renderButtons() {
 
-        // Deleting the movie buttons prior to adding new movie buttons
-        // (this is necessary otherwise we will have repeat buttons)
+        // Deleting the buttons prior to adding new buttons
         $("#topics-view").empty();
 
-        // Looping through the array of movies
+        // Looping through the array of topics
         for (var i = 0; i < topics.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class
-          a.addClass("topic");
-          // Adding a data-attribute with a value of the movie at index i
-          a.attr("data-name", topics[i]);
-          // Providing the button's text with a value of the movie at index i
+            // Then dynamicaly generating buttons for each topic in the array.
+            var a = $("<button>");
+            // Adding a class
+            a.addClass("topic");
+            // Adding a data-attribute with a value of the topic at index i
+            a.attr("data-name", topics[i]);
+            // Providing the button's text with a value of the topic at index i
 
-          // var a = $("<button class=movie data-name=''").attr("data-name", movies[i]);
-          a.text(topics[i]);
-          // Adding the button to the HTML
-          $("#topics-view").append(a);
+            a.text(topics[i]);
+            // Adding the button to the HTML
+            $("#topics-view").append(a);
+
+
         }
-      }
+    }
 
-      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=dc6zaTOxFJmzC";
+    function generateOnClick() {
+        var dogs = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + dogs + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-      console.log(topics);
+        console.log(topics);
 
-      // Creating an AJAX call for the specific movie button being clicked
+        // Creating an AJAX call for the specific button being clicked
         $.ajax({
-          url: queryURL,
-          method: "GET"
+            url: queryURL,
+            method: "GET"
         }).done(function(response) {
 
-          console.log(queryURL);
+            console.log(response);
 
-          var results = response.data;
+            var results = response.data;
+            for (var i = 0; i < results.length; i++) {
 
-for (var i = 0; i < results.length; i++) {
 
-  var dogDiv = $('<div>');
 
-                        var p = $('<p>').text("Rating: " + results[i].rating);
+                var dogDiv = $('<div class="dog-class" title="Click on gif to start and stop animation">');
 
-                        var dogsImage = $('<img>');
-                        dogImage.attr('src', results[i].images.fixed_height.url);
 
-                        dogDiv.append(p);
-                        dogDiv.prepend(dogImage);
+                var p = $('<p>').text("Rating: " + results[i].rating);
 
-                        $('#gifSection').prepend(dogDiv);
 
-                      }
+                var dogImage = $('<img>').on("click", function() {
+                    var state = $(this).attr("data-state");
+                    console.log(state);
 
-               
-       });
+                    if (state == "still") {
+                        var changeState = $(this).attr("data-animate");
+                        $(this).attr("src", changeState);
+                        $(this).attr("data-state", "animate");
+                    }
 
-      // This function handles events where one button is clicked
-      $("#add-topic").on("click", function(event) {
+                    if (state == "animate") {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+
+
+                    }
+                });
+                dogImage.attr('src', results[i].images.fixed_height.url);
+                dogImage.attr('data-still', results[i].images.fixed_height_still.url);
+                dogImage.attr('data-state', 'still');
+                dogImage.addClass('gif');
+                dogImage.attr('data-animate', results[i].images.fixed_height.url);
+
+                dogDiv.append(p);
+                dogDiv.prepend(dogImage);
+                console.log(dogImage, dogDiv)
+                $('#gifSection').prepend(dogDiv);
+
+            }
+
+
+
+
+        });
+
+    }
+
+    // This function handles events where one button is clicked
+    $("#add-topic").on("click", function(event) {
         // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
         event.preventDefault();
 
         // This line will grab the text from the input box
         var topic = $("#topic-input").val().trim();
-        // The movie from the textbox is then added to our array
+        // The topic from the textbox is then added to our array
         topics.push(topic);
 
         // calling renderButtons which handles the processing of our movie array
         renderButtons();
-      });
+    });
 
-      // Calling the renderButtons function at least once to display the initial list of movies
-      renderButtons();
+    // Calling the renderButtons function at least once to display the initial list of topics
+    renderButtons();
+
+    $(document).on("click", ".topic", generateOnClick);
+    renderButtons();
 
 
 
-      
-    })
-    
-
+})
